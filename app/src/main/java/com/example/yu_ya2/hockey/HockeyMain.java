0ae04a4540a434963fdc3fd9040640b7b807caca
asanoyu <s15t201@stu.kagawa-u.ac.jp>
2016-12-01 21:06:40 +0900

    make git rep

---
+++ app/src/main/java/com/example/yu_ya2/hockey/HockeyMain.java
@@ -0,0 +1,13 @@
+package com.example.yu_ya2.hockey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HockeyMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hockey_main);
+    }
+}
